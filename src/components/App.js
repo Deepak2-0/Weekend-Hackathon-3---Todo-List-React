f44a2292c292318c25ae550c6354b7e4d2e54f16
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-10-31 20:32:01 +0530

    Initial Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,111 @@
 import React from "react";
 import "./../styles/App.css";
+import ToDoItem from "./ToDoItem";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+function App() {
+  const [inputText, setInputText] = React.useState("");
+  const [items, setItems] = React.useState([]);
+  const [display, setDisplay] = React.useState([]);
+
+  let keyCount = 1;
+
+  function handleChange(event) {
+    let text = event.target.value;
+    setInputText(text);
+  }
+
+  function addItem() {
+    if (inputText.length === 0) return;
+
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+
+    // setDisplay((prevDisplay) => {
+    //   return [...prevDisplay, false];
+    // });
+
+    let arr = [...display];
+
+    arr.push(false);
+    setDisplay(arr);
+
+    setInputText("");
+    //console.log("check add");
+  }
+
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((el, index) => {
+        return index !== id;
+      });
+    });
+
+    let arr = [...display];
+    arr.splice(id, 1);
+    setDisplay(arr);
+
+    // setDisplay((prevDisplay) => {
+    //   return prevDisplay.filter((el, index) => {
+    //     return index !== id;
+    //   });
+    // });
+
+    //console.log("check");
+  }
+  function handleEdit(id) {
+    //setDisplay((prevDisplay, index) => {
+    //   return (prevDisplay = index === id);
+    // });
+
+    let arr = [...display];
+    arr[id] = true;
+    setDisplay(arr);
+    //console.log(display);
+  }
+  let editableText;
+
+  function onEditText(event) {
+    editableText = event.target.value;
+  }
+
+  function onEditSave(id) {
+    let arr = [...display];
+    arr[id] = false;
+    setDisplay(arr);
+
+    let textArr = [...items];
+    textArr[id] = editableText;
+
+    setItems(textArr);
+  }
+  return (
+    <div id="main">
+      <div id="task">
+        <input type="text" onChange={handleChange} value={inputText} />
+        <button id="btn" onClick={addItem}>
+          Add
+        </button>
+      </div>
+
+      <div>
+        <ul>
+          {items.map((el, index) => (
+            <ToDoItem
+              onDelete={deleteItem}
+              key={keyCount++}
+              id={index}
+              item={el}
+              onEdit={handleEdit}
+              onShow={display[index]}
+              onEditText={onEditText}
+              onEditSave={onEditSave}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
-
 
 export default App;
---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function ToDoItem(props) {
+  const { onDelete, id, item, onEdit, onShow, onEditText, onEditSave } = props;
+  return (
+    <div className="list">
+      <li>{item}</li>
+      <div>
+        <button onClick={() => onEdit(id)} className="edit">
+          {" "}
+          edit
+        </button>
+        <button onClick={() => onDelete(id)} className="delete">
+          delete
+        </button>
+      </div>
+      {onShow && (
+        <div>
+          <input
+            type="text"
+            className="editTask"
+            defaultValue={item}
+            onChange={onEditText}
+          />
+          <button className="saveTask" onClick={() => onEditSave(id)}>
+            Save
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
